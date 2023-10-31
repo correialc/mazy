51d15e83450797c833be6c59031bfa56292a8dad
Leandro Correia <leandro.correia@gmail.com>
2023-10-31 17:34:33 -0300

    Create models for cell roles and borders

---
+++ src/mazy/models/cell.py
@@ -0,0 +1,34 @@
+from enum import IntEnum, IntFlag, auto
+
+
+class CellRole(IntEnum):
+    NONE = 0
+    ENTRANCE = auto()
+    EXIT = auto()
+    EXTERIOR = auto()
+    WALL = auto()
+
+
+class Border(IntFlag):
+    EMPTY = 0
+    TOP = auto()
+    BOTTOM = auto()
+    LEFT = auto()
+    RIGHT = auto()
+
+    @property
+    def corner(self) -> bool:
+        return self in (
+            self.TOP | self.LEFT,
+            self.TOP | self.RIGHT,
+            self.BOTTOM | self.LEFT,
+            self.BOTTOM | self.RIGHT,
+        )
+
+    @property
+    def dead_end(self) -> bool:
+        return self.bit_count() == 3
+
+    @property
+    def intersection(self) -> bool:
+        return self.bit_count() < 2
--- tests/test_placeholder.py
+++ tests/test_placeholder.py
@@ -1,3 +1,54 @@
-def test_cell_border() -> None:
-    """Test placeholder"""
-    assert True
+from typing import Sequence
+from pytest import fixture
+
+from mazy.models.cell import Border
+
+
+@fixture
+def cell_borders() -> Sequence[Border]:
+    return [
+        Border.TOP,
+        Border.BOTTOM,
+        Border.LEFT,
+        Border.RIGHT,
+        Border.TOP | Border.LEFT,
+        Border.TOP | Border.RIGHT,
+        Border.BOTTOM | Border.LEFT,
+        Border.BOTTOM | Border.RIGHT,
+        Border.TOP | Border.BOTTOM,
+        Border.LEFT | Border.RIGHT,
+        Border.TOP | Border.BOTTOM | Border.LEFT,
+        Border.TOP | Border.BOTTOM | Border.RIGHT,
+        Border.TOP | Border.LEFT | Border.RIGHT,
+        Border.BOTTOM | Border.LEFT | Border.RIGHT,
+        Border.BOTTOM | Border.LEFT | Border.RIGHT | Border.LEFT,
+    ]
+
+
+def test_cell_border_corner(cell_borders: Sequence[Border]) -> None:
+    """Corner cells should have exactly 2 consecutive borders."""
+    for border in cell_borders:
+        if not border.bit_count() == 2 or border in [
+            Border.TOP | Border.BOTTOM,
+            Border.LEFT | Border.RIGHT,
+        ]:
+            assert border.corner is False
+        else:
+            assert border.corner is True
+
+
+def test_cell_border_dead_end(cell_borders: Sequence[Border]) -> None:
+    """Dead end cells should have exactly 3 borders."""
+    for border in cell_borders:
+        if border.bit_count() == 3:
+            assert border.dead_end is True
+        else:
+            assert border.dead_end is False
+
+
+def test_cell_border_intersection(cell_borders: Sequence[Border]) -> None:
+    for border in cell_borders:
+        if border.bit_count() < 2:
+            assert border.intersection is True
+        else:
+            assert border.intersection is False
