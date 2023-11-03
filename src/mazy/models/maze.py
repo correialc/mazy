164903a89766981a5d69fecd2b289f9482884599
Leandro Correia <leandro.correia@gmail.com>
2023-11-03 18:08:14 -0300

    Add a method to registry neighbors when creating the maze

--- src/mazy/models/maze.py
+++ src/mazy/models/maze.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Sequence
 
-from mazy.models.cell import Cell
+from mazy.models.cell import Cell, Role, Direction
 
 
 class MazeState(Enum):
@@ -17,7 +17,26 @@ class Maze:
         self.cells: Sequence[Sequence[Cell]] = [
             [Cell(row, col) for col in range(cols)] for row in range(rows)
         ]
+        self.registry_neighbors()
 
     def __getitem__(self, index: tuple[int, int]) -> Cell:
         i, j = index
         return self.cells[i][j]
+
+    def registry_neighbors(self) -> None:
+        for row in range(self.rows):
+            for col in range(self.cols):
+                cell = self[row, col]
+
+                if cell.row == 0 and cell.col == 0:
+                    cell.role = Role.ENTRANCE
+
+                if not cell.col == 0:
+                    cell.link_to(
+                        self[row, col - 1], passage=False, direction=Direction.WEST
+                    )
+
+                if not cell.row == 0:
+                    cell.link_to(
+                        self[row - 1, col], passage=False, direction=Direction.NORTH
+                    )
--- tests/models/test_maze.py
+++ tests/models/test_maze.py
@@ -1,5 +1,6 @@
 import pytest
 
+from mazy.models.cell import Role, Direction
 from mazy.models.maze import Maze, MazeState
 
 
@@ -17,3 +18,35 @@ def test_maze_cell_get_accessor() -> None:
     cell = maze[0, 1]
     assert cell.row == 0
     assert cell.col == 1
+
+
+def test_maze_registry_neighbors() -> None:
+    """Should generate a valid maze.
+
+    Entrance cell must be in the origin [0,0]
+    Frontier cells should not have neighbors beyond their limits
+    """
+    maze = Maze(rows=3, cols=3)
+
+    origin_cell = maze[0, 0]
+
+    assert origin_cell.role == Role.ENTRANCE
+    assert len(origin_cell.neighbors) == 2
+    assert Direction.NORTH not in origin_cell.neighbors
+    assert Direction.WEST not in origin_cell.neighbors
+    assert Direction.SOUTH in origin_cell.neighbors
+    assert Direction.EAST in origin_cell.neighbors
+
+    middle_cell = maze[1, 1]
+    assert len(middle_cell.neighbors) == 4
+    assert Direction.NORTH in middle_cell.neighbors
+    assert Direction.WEST in middle_cell.neighbors
+    assert Direction.SOUTH in middle_cell.neighbors
+    assert Direction.EAST in middle_cell.neighbors
+
+    corner_cell = maze[2, 2]
+    assert len(origin_cell.neighbors) == 2
+    assert Direction.NORTH in corner_cell.neighbors
+    assert Direction.WEST in corner_cell.neighbors
+    assert Direction.SOUTH not in corner_cell.neighbors
+    assert Direction.EAST not in corner_cell.neighbors
