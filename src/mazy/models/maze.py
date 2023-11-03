b6c7e490ff5388e4a1bd8d1dc11d9b9ade4249ad
Leandro Correia <leandro.correia@gmail.com>
2023-11-03 15:28:50 -0300

    Make the maze get accessor bidimensional This makes possible to get rid of the cell index calculation and do the cell look up using row and column indexes. The code become considerably simpler  after that.

--- src/mazy/models/maze.py
+++ src/mazy/models/maze.py
@@ -14,29 +14,10 @@ class MazeState(Enum):
 class Maze:
     def __init__(self, rows: int, cols: int):
         self.state = MazeState.BUILDING
-        self.cells = tuple(
-            [Cell(row, col) for row in range(rows) for col in range(cols)]
-        )
+        self.rows = rows
+        self.cols = cols
+        self.cells = [[Cell(row, col) for col in range(cols)] for row in range(rows)]
 
-    def __iter__(self) -> Iterator[Cell]:
-        return iter(self.cells)
-
-    def __getitem__(self, index: int) -> Cell:
-        return self.cells[index]
-
-    @cached_property
-    def width(self) -> int:
-        return max(cell.col for cell in self) + 1
-
-    @cached_property
-    def height(self) -> int:
-        return max(cell.row for cell in self) + 1
-
-    def get_cell(self, row: int, col: int) -> Cell:
-        index = calculate_cell_index(self.width, row, col)
-        return self[index]
-
-
-def calculate_cell_index(maze_width: int, cell_row: int, cell_col: int) -> int:
-    """Calculate the cell index based on its row and col."""
-    return maze_width * cell_row + cell_col
+    def __getitem__(self, index: tuple) -> Cell:
+        i, j = index
+        return self.cells[i][j]
--- tests/models/test_maze.py
+++ tests/models/test_maze.py
@@ -1,34 +1,19 @@
 import pytest
 
-from mazy.models.maze import Maze, MazeState, calculate_cell_index
+from mazy.models.maze import Maze, MazeState
 
 
 def test_maze_init() -> None:
     """Must generate a maze with a proper size and state."""
     maze = Maze(rows=2, cols=3)
     assert maze.state == MazeState.BUILDING
-    assert maze.width == 3
-    assert maze.height == 2
+    assert maze.rows == 2
+    assert maze.cols == 3
 
 
-def test_maze_calculate_cell_index() -> None:
-    """Should calculate a valid index for the given cell row and col."""
+def test_maze_cell_get_accessor() -> None:
+    """Ensure individual cells of the maze can be accessed by row and col."""
     maze = Maze(rows=2, cols=3)
-    assert calculate_cell_index(maze.width, cell_row=0, cell_col=0) == 0
-    assert calculate_cell_index(maze.width, cell_row=0, cell_col=1) == 1
-    assert calculate_cell_index(maze.width, cell_row=0, cell_col=2) == 2
-    assert calculate_cell_index(maze.width, cell_row=1, cell_col=0) == 3
-    assert calculate_cell_index(maze.width, cell_row=1, cell_col=1) == 4
-
-
-@pytest.mark.parametrize("row", [0, 1])
-@pytest.mark.parametrize("col", [0, 1, 2])
-def test_maze_get_cell(
-    row: int,
-    col: int,
-) -> None:
-    """Must return a cell for a given row and col."""
-    maze = Maze(rows=2, cols=3)
-    cell = maze.get_cell(row, col)
-    assert cell.row == row
-    assert cell.col == col
+    cell = maze[0, 1]
+    assert cell.row == 0
+    assert cell.col == 1
