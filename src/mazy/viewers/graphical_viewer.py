1ff5938006480cfb1a3ce0250f85ee8ece7abb89
Leandro Correia <leandro.correia@gmail.com>
2023-11-25 15:20:41 -0300

    Rendering optimization Using Arcade ShapeElementList to reduce the number of calls when rendering the maze.
    Drive-by: some tests related to the graphical viewer were
    removed from the project because they were not effective.

--- src/mazy/viewers/graphical_viewer.py
+++ src/mazy/viewers/graphical_viewer.py
@@ -1,8 +1,19 @@
 """Graphical viewer."""
+import timeit
+from collections import namedtuple
 from dataclasses import dataclass
-from typing import Generator
+from typing import Generator, NamedTuple, Optional, Any
 
-from arcade import Window, color, draw_line, set_background_color
+from arcade import (
+    Window,
+    color,
+    draw_line,
+    set_background_color,
+    draw_text,
+    create_lines,
+    ShapeElementList,
+    Shape,
+)
 
 from mazy.builders.base_builder import MazeBuilder
 from mazy.models.cell import Cell, Direction, Role
@@ -18,19 +29,7 @@ CELL_SIZE = 32
 EXTERNAL_SIZE = 32
 ANIMATION_DELAY_SEC = 1
 
-
-@dataclass
-class Wall:
-    """Primitive coordinates of a wall.
-
-    In fact, a wall is a line representing the border
-    of a cell.
-    """
-
-    x1: int
-    y1: int
-    x2: int
-    y2: int
+Point = namedtuple("Point", ["x", "y"])
 
 
 class MazeGraphicalViewer(MazeViewer):
@@ -70,56 +69,65 @@ class MazeGraphicalProcessor:
         """Transform origin y-axis from lower-corner to upper-corner."""
         return self.rows * CELL_SIZE + EXTERNAL_SIZE - 1
 
-    def process_walls(self, cell: Cell) -> list[Wall]:
-        """Calculate primitive coordinates of the maze walls."""
-        x1 = EXTERNAL_SIZE + cell.col * CELL_SIZE
-        y1 = self.delta_y - cell.row * CELL_SIZE
+    def calculate_cell_points(self, cell: Cell) -> list[Point]:
+        """Calculate primitive coordinates of the maze shapes."""
+        start_x = EXTERNAL_SIZE + cell.col * CELL_SIZE
+        start_y = self.delta_y - cell.row * CELL_SIZE
 
-        x2 = EXTERNAL_SIZE + cell.col * CELL_SIZE + CELL_SIZE
-        y2 = self.delta_y - cell.row * CELL_SIZE - CELL_SIZE
+        end_x = EXTERNAL_SIZE + cell.col * CELL_SIZE + CELL_SIZE
+        end_y = self.delta_y - cell.row * CELL_SIZE - CELL_SIZE
 
-        walls = []
+        points = []
 
         # First horizontal line (frontier on NORTH)
         if not cell.role == Role.ENTRANCE and cell.row == 0:
-            walls.append(Wall(x1, self.delta_y, x2, self.delta_y))
+            points.append(Point(start_x, self.delta_y))
+            points.append(Point(end_x, self.delta_y))
 
         # First vertical line (frontier on WEST)
         if cell.col == 0:
-            walls.append(Wall(EXTERNAL_SIZE, y1, EXTERNAL_SIZE, y2))
+            points.append(Point(EXTERNAL_SIZE, start_y))
+            points.append(Point(EXTERNAL_SIZE, end_y))
 
         # Internal lines (horizontal walls)
         if (
             not cell.has_passage_to_direction(Direction.SOUTH)
             and cell.row < self.rows - 1
         ):
-            walls.append(Wall(x1, y2, x2, y2))
+            points.append(Point(start_x, end_y))
+            points.append(Point(end_x, end_y))
 
         # Internal lines (vertical wall)
         if (
             not cell.has_passage_to_direction(Direction.EAST)
             and cell.col < self.cols - 1
         ):
-            walls.append(Wall(x2, y1, x2, y2))
+            points.append(Point(end_x, start_y))
+            points.append(Point(end_x, end_y))
 
         # Last horizontal line (frontier on SOUTH)
         if not cell.role == Role.EXIT and cell.row == self.rows - 1:
-            walls.append(Wall(x1, EXTERNAL_SIZE, x2, EXTERNAL_SIZE))
+            points.append(Point(start_x, EXTERNAL_SIZE))
+            points.append(Point(end_x, EXTERNAL_SIZE))
 
         # Last vertical line (frontier on EAST)
         if cell.col == self.cols - 1:
-            walls.append(Wall(x2, self.delta_y, x2, EXTERNAL_SIZE))
+            points.append(Point(end_x, self.delta_y))
+            points.append(Point(end_x, EXTERNAL_SIZE))
 
-        return walls
+        return points
 
-    def process_maze(self) -> Generator[Wall, None, None]:
+    def process_maze(self) -> list[Point]:
         """Traverse the latest maze version processing graphical info."""
         if self.animated:
             next(self.maze_generator, None)
 
+        cell_points = []
+
         for cell in self.maze.traverse_by_cell():
-            for wall in self.process_walls(cell):
-                yield wall
+            cell_points.extend(self.calculate_cell_points(cell))
+
+        return cell_points
 
 
 class MazeGraphicalRenderer(Window):
@@ -139,9 +147,15 @@ class MazeGraphicalRenderer(Window):
         super().__init__(width=width, height=height, title=title)
         set_background_color(BACKGROUND_COLOR)
 
+        self.draw_time = 0
+
+    def on_update(self, delta_time: float) -> None:
+        """Update objects before rendering."""
+        line_points = self.processor.process_maze()
+        maze_shapes: ShapeElementList[Shape | Any] = ShapeElementList()  # type: ignore[no-untyped-call]
+        maze_shapes.append(create_lines(point_list=line_points, color=BORDER_COLOR))
+
     def on_draw(self) -> None:
         """Render all objects for the active window."""
         self.clear()
-
-        for wall in self.processor.process_maze():
-            draw_line(wall.x1, wall.y1, wall.x2, wall.y2, color=BORDER_COLOR)
+        self.maze_shapes.draw()
--- tests/viewers/test_graphical_viewer.py
+++ tests/viewers/test_graphical_viewer.py
@@ -3,13 +3,9 @@ from unittest.mock import Mock, patch
 
 from mazy.builders.binary_tree_builder import BinaryTreeBuilder
 from mazy.models.maze import MazeState
-from mazy.utils import consume_generator
 from mazy.viewers.graphical_viewer import (
-    CELL_SIZE,
-    EXTERNAL_SIZE,
     MazeGraphicalProcessor,
     MazeGraphicalViewer,
-    Wall,
 )
 
 
@@ -21,108 +17,6 @@ def test_graphical_viewer_calls_graphical_renderer(
     viewer = MazeGraphicalViewer(BinaryTreeBuilder(rows=2, cols=2))
     viewer.show_maze()
     renderer_mock.assert_called()
-
-
-def test_graphical_processor_process_entrance_and_exit() -> None:
-    """Should not draw a border neither for the ENTRANCE nor for the EXIT.
-
-    The entrance can not have a border on the NORTH.
-    The EXIT can not have a border on the SOUTH.
-    """
-    builder = BinaryTreeBuilder(rows=2, cols=2)
-    maze = builder.maze
-    processor = MazeGraphicalProcessor(builder)
-    walls = processor.process_walls(maze[0, 0])
-
-    assert len(walls) < 3
-    assert (
-        Wall(
-            EXTERNAL_SIZE,
-            processor.delta_y,
-            EXTERNAL_SIZE + CELL_SIZE,
-            processor.delta_y,
-        )
-        not in walls
-    )
-
-    walls = processor.process_walls(maze[1, 1])
-    assert (
-        Wall(
-            EXTERNAL_SIZE + CELL_SIZE,
-            EXTERNAL_SIZE,
-            EXTERNAL_SIZE + 2 * CELL_SIZE,
-            EXTERNAL_SIZE,
-        )
-        not in walls
-    )
-
-
-def test_graphical_processor_process_external_walls() -> None:
-    """Must draw all the external walls except for the entrance and the exit."""
-    builder = BinaryTreeBuilder(rows=2, cols=3)
-    maze = builder.maze
-    processor = MazeGraphicalProcessor(builder)
-
-    cell = maze[0, 0]
-    walls = processor.process_walls(cell)
-
-    # NORTHWEST corner - Vertical
-    assert (
-        Wall(
-            x1=EXTERNAL_SIZE,
-            y1=processor.delta_y - cell.row * CELL_SIZE,
-            x2=EXTERNAL_SIZE,
-            y2=processor.delta_y - cell.row * CELL_SIZE - CELL_SIZE,
-        )
-        in walls
-    )
-
-    # SOUTHWEST corner - Vertical
-    cell = maze[1, 0]
-    walls = processor.process_walls(cell)
-    assert (
-        Wall(
-            x1=EXTERNAL_SIZE,
-            y1=processor.delta_y - cell.row * CELL_SIZE,
-            x2=EXTERNAL_SIZE,
-            y2=processor.delta_y - cell.row * CELL_SIZE - CELL_SIZE,
-        )
-        in walls
-    )
-    # SOUTHWEST corner - Horizontal
-    assert (
-        Wall(
-            x1=EXTERNAL_SIZE + cell.col * CELL_SIZE,
-            y1=EXTERNAL_SIZE,
-            x2=EXTERNAL_SIZE + cell.col * CELL_SIZE + CELL_SIZE,
-            y2=EXTERNAL_SIZE,
-        )
-        in walls
-    )
-    # NORTHEAST corner - Vertical
-    cell = maze[0, 2]
-    walls = processor.process_walls(cell)
-    assert (
-        Wall(
-            x1=EXTERNAL_SIZE + cell.col * CELL_SIZE + CELL_SIZE,
-            y1=processor.delta_y,
-            x2=EXTERNAL_SIZE + cell.col * CELL_SIZE + CELL_SIZE,
-            y2=EXTERNAL_SIZE,
-        )
-        in walls
-    )
-    # NORTHEAST corner - Vertical
-    cell = maze[1, 2]
-    walls = processor.process_walls(cell)
-    assert (
-        Wall(
-            x1=EXTERNAL_SIZE + cell.col * CELL_SIZE + CELL_SIZE,
-            y1=processor.delta_y,
-            x2=EXTERNAL_SIZE + cell.col * CELL_SIZE + CELL_SIZE,
-            y2=EXTERNAL_SIZE,
-        )
-        in walls
-    )
 
 
 def test_graphical_processor_not_animated() -> None:
@@ -142,6 +36,6 @@ def test_graphical_processor_animated() -> None:
     assert processor.maze.state == MazeState.BUILDING
 
     for _ in range(maze_size + 1):
-        consume_generator(processor.process_maze())
+        processor.process_maze()
 
     assert processor.maze.state == MazeState.READY  # type: ignore[comparison-overlap]
