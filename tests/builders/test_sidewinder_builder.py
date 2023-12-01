f7dae9624c7172f8ec3b21af1e519effcb6857b6
Leandro Correia <leandro.correia@gmail.com>
2023-12-01 19:30:57 -0300

    Release v0.2.0 preparation - Remove flake8 config file - Update README - Fix flaky test

--- tests/builders/test_sidewinder_builder.py
+++ tests/builders/test_sidewinder_builder.py
@@ -1,27 +1,28 @@
 """Tests for the binary tree builder."""
+
 from mazy.builders.sidewinder import SidewinderBuilder
 from mazy.models.builder import BuilderAlgorithm
 from mazy.models.maze import MazeState
 from mazy.utils import consume_generator
 
 
-def test_binary_tree_builder_default_values() -> None:
+def test_sidewinder_builder_default_values() -> None:
     """Ensure default values are consistent."""
     builder = SidewinderBuilder(rows=3, cols=5)
 
     assert builder.name == BuilderAlgorithm.SIDEWINDER.value
 
 
-def test_binary_tree_builder_build_maze() -> None:
+def test_sidewinder_builder_build_maze() -> None:
     """Ensure each cell has at least 1 and no more than 3 passages."""
-    builder = SidewinderBuilder(rows=3, cols=5)
+    builder = SidewinderBuilder(rows=5, cols=8)
     maze = consume_generator(builder.build_maze())
 
     for cell in maze.traverse_by_cell():
-        assert 0 < cell.passage_count() <= 3
+        assert 0 < cell.passage_count() <= 4
 
 
-def test_binary_tree_builder_build_maze_set_visited_flag() -> None:
+def test_sidewinder_builder_build_maze_set_visited_flag() -> None:
     """Should set the visited set for each visited cell."""
     builder = SidewinderBuilder(rows=3, cols=5)
 
@@ -34,7 +35,7 @@ def test_binary_tree_builder_build_maze_set_visited_flag() -> None:
         assert cell.visited is True
 
 
-def test_binary_tree_builder_build_maze_manages_maze_states() -> None:
+def test_sidewinder_builder_build_maze_manages_maze_states() -> None:
     """Ensure the builder set the correct state for each maze building step."""
     maze_generator = SidewinderBuilder(rows=3, cols=5).build_maze()
 
